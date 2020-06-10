9a14b5977a9ff201dfdbd5ad0beecdc881e913f5
Ruud, Xu <ruud.xu@sap.com>
2020-06-10 16:32:00 +0800

    Week8

--- Week_08/NOTE.md
+++ Week_08/NOTE.md
@@ -209,5 +209,23 @@
     ```
   
 [排序算法练手](https://www.google.com)
+#### 选择排序
+```java
+    public static int[] order(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int minIndex = i;
+            for (int j = i; j < nums.length; j++) {
+                if (nums[minIndex] > nums[j]) {
+                    minIndex = j;
+                }
+            }
+            int tmp = nums[i];
+            nums[i] = nums[minIndex];
+            nums[minIndex] = tmp;
+        }
+        return nums;
+    }
+```
+
 #### 插入排序
 
--- Week_08/order_practices/SelectionOrder.java
+++ Week_08/order_practices/SelectionOrder.java
@@ -3,7 +3,7 @@ package Week_08.order_practices;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class InsertOrder {
+public class SelectionOrder {
     public static int[] order(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             int minIndex = i;
@@ -21,10 +21,10 @@ public class InsertOrder {
 
     @Test
     public void test_practices() {
-        int[] result = InsertOrder.order(new int[]{4,3,8,1,0,9,5,7,2,6});
+        int[] result = SelectionOrder.order(new int[]{4,3,8,1,0,9,5,7,2,6});
         Assert.assertArrayEquals(new int[]{0,1,2,3,4,5,6,7,8,9}, result);
 
-        result = InsertOrder.order(new int[]{4,3,8,1,0,9,5,0,7,2,0,6});
+        result = SelectionOrder.order(new int[]{4,3,8,1,0,9,5,0,7,2,0,6});
         Assert.assertArrayEquals(new int[]{0,0,0,1,2,3,4,5,6,7,8,9}, result);
     }
 }
